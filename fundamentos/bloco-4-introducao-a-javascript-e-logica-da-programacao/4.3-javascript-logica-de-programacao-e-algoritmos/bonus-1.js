bba34a8d4894a9bfcd222414dc8cce718e742f78
Natalia Augusto <nat_augusto@hotmail.com>
2022-05-19 19:54:45 -0300

    Exercício bônus 1 do dia 4.4 resolvido

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-da-programacao/4.3-javascript-logica-de-programacao-e-algoritmos/bonus-1.js
@@ -0,0 +1,10 @@
+let numero = 5;
+let linha = ''
+
+for (let indice = 0; indice < numero; indice += 1) {
+	linha += '*';
+}
+
+for (let indice = 0; indice < numero; indice += 1) {
+	console.log(linha);
+}
