f4cdb0a6b1467d057a4df2f8316231e4e8dc62e5
Natalia Augusto <nat_augusto@hotmail.com>
2022-05-31 22:11:58 -0300

    Requisitos 1 e 2 refatorados; Requisito 3 - Adicionado evento para mudar a cor de fundo dos feriados ao clicar no botão feriados

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/5.3-javascript-eventos/exercicios/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/5.3-javascript-eventos/exercicios/script.js
@@ -15,39 +15,63 @@ createDaysOfTheWeek();
   
 // Escreva seu código abaixo.
 //Exercício 1
+let dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
 function createDezDays() {
-  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
-  const daysList = document.querySelector('#days');
+  let daysList = document.querySelector('#days');
 
   for (let index = 0; index < dezDaysList.length; index += 1) {
-    const day = dezDaysList[index];
-    const daysListItem = document.createElement('li');
-    daysListItem.innerHTML = day;
-        
-    if (day === 4 || day === 11 || day === 18) {
-      daysListItem,className = 'day friday';
-      daysList.appendChild(daysListItem);
-    } else if (day === 24 || day === 31) {
-      daysListItem,className = 'day holiday';
-      daysList.appendChild(daysListItem);
+    let day = dezDaysList[index];
+    let dayItem = document.createElement('li');
+
+    if (day === 24 || day === 31) {
+      dayItem.className = 'day holiday';
+      dayItem.innerHTML = day;
+      daysList.appendChild(dayItem);
+    } else if (day === 4 || day === 11 || day === 18) {
+      dayItem.className = 'day friday';
+      dayItem.innerHTML = day;
+      daysList.appendChild(dayItem);
     } else if (day === 25) {
-      daysListItem,className = 'day friday holiday';
-      daysList.appendChild(daysListItem);
+      dayItem.className = 'day holiday friday';
+      dayItem.innerHTML = day;
+      daysList.appendChild(dayItem);
     } else {
-      daysListItem,className = 'day';
-      daysList.appendChild(daysListItem);
+      dayItem.innerHTML = day;
+      dayItem.className = 'day';
+      daysList.appendChild(dayItem);
     }
-  }
-}
+  };
+};
+createDezDays();
 
 //Exercício 2
-createDezDays()
+function createHolidayButton() {
+  let buttonContainer = document.querySelector('.buttons-container');
+  let newButton = document.createElement('button');
 
-function createHolidaysButton(buttonName) {
-  const buttonsContainer = document.querySelector('.buttons-container');
-  const holidaysButton = document.createElement('button');
-  holidaysButton.innerHTML = buttonName
-  holidaysButton.id = 'btn-holiday';
-  buttonsContainer.appendChild(holidaysButton);
+  newButton.innerHTML = 'Feriados';
+  newButton.id = 'btn-holiday';
+  buttonContainer.appendChild(newButton);
+};
+
+createHolidayButton();
+
+//Exercício 3
+function showHolidays() {
+  let holidaysButton = document.querySelector('#btn-holiday');
+  let holidays = document.querySelectorAll('.holiday');
+  let backgroundColor = 'rgb(238,238,238)';
+  let newBackgroundColor = 'violet';
+  
+  holidaysButton.addEventListener('click', function () {
+    for (let index = 0; index < holidays.length; index += 1) {
+      if (holidays[index].style.backgroundColor === newBackgroundColor) {
+        holidays[index].style.backgroundColor = backgroundColor;
+      } else {
+        holidays[index].style.backgroundColor = newBackgroundColor;
+      }
+    }
+  })
 }
-createHolidaysButton('Feriados');
+showHolidays();
