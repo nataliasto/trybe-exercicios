6bf35f98ca987bad87ad39206b548e320b30a369
Natalia Augusto <nat_augusto@hotmail.com>
2022-10-14 18:52:39 -0300

    Setup do exercício

---
+++ front-end/secao-9-context-api-e-react-hooks/9.1-context-api-do-react/fixacao/context-color-box/src/App.js
@@ -0,0 +1,14 @@
+// ./App.js
+import './App.css';
+import React from 'react';
+import ColorBox from './components/ColorBox';
+
+class App extends React.Component {
+  render() {
+    return (
+      <ColorBox />
+    );
+  }
+}
+
+export default App;
---
+++ front-end/secao-9-context-api-e-react-hooks/9.1-context-api-do-react/fixacao/context-color-box/src/components/ColorBox.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/box.css';
+
+class ColorBox extends React.Component {
+  render() {
+    return(
+      <button
+        type="button"
+        className="box"
+        style={ { backgroundColor: 'blue' } }
+      >
+        Click me to change my color!
+      </button>
+    )
+  }
+}
+
+export default ColorBox;
