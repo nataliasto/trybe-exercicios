5686c1bb6427a2b7250035e8ad09f09479d619b2
Natalia Augusto <nat_augusto@hotmail.com>
2022-10-15 17:08:22 -0300

    Exercício 1 - Mudar a cor de fundo do botão usando context api

--- front-end/secao-9-context-api-e-react-hooks/9.1-context-api-do-react/fixacao/context-color-box/src/App.js
+++ front-end/secao-9-context-api-e-react-hooks/9.1-context-api-do-react/fixacao/context-color-box/src/App.js
@@ -1,12 +1,42 @@
-// ./App.js
-import './App.css';
 import React from 'react';
 import ColorBox from './components/ColorBox';
+import Context from './context/Context';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      btnBgColors: ['red', 'yellow', 'blue'],
+      actualBtnBgColor: 'blue',
+    }
+  }
+
+  changeColor = () => {
+    const { btnBgColors, actualBtnBgColor } = this.state;
+    if (actualBtnBgColor === 'red') {
+      this.setState({actualBtnBgColor: btnBgColors[1]})
+    } else if (actualBtnBgColor === 'yellow') {
+      this.setState({actualBtnBgColor: btnBgColors[2]})
+    } else {
+    this.setState({actualBtnBgColor: btnBgColors[0]})
+    }
+  }
+
+  handleClick = (event) => {
+    event.preventDefault();
+    this.changeColor()    
+  }
+
   render() {
+    const { actualBtnBgColor } = this.state;
+    const contextValue = {
+      actualBtnBgColor,
+      handleClick: this.handleClick,
+    }
     return (
-      <ColorBox />
+      <Context.Provider value={contextValue}>
+        <ColorBox />
+      </Context.Provider>
     );
   }
 }
--- front-end/secao-9-context-api-e-react-hooks/9.1-context-api-do-react/fixacao/context-color-box/src/components/ColorBox.js
+++ front-end/secao-9-context-api-e-react-hooks/9.1-context-api-do-react/fixacao/context-color-box/src/components/ColorBox.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import '../styles/box.css';
+import Context from '../context/Context'
 
 class ColorBox extends React.Component {
   render() {
+    const { actualBtnBgColor, handleClick } = this.context;
     return(
       <button
         type="button"
         className="box"
-        style={ { backgroundColor: 'blue' } }
+        style={ { backgroundColor: actualBtnBgColor } }
+        onClick={handleClick}
       >
         Click me to change my color!
       </button>
     )
   }
 }
+
+ColorBox.contextType = Context;
 
 export default ColorBox;
