6d71dfb00b110e14e94e04c9ecb90b448d848e71
Natalia Augusto <nat_augusto@hotmail.com>
2022-11-23 23:29:12 -0300

    Para Fixar - Manipulação de strings

---
+++ back-end/secao-3-funcoes-sql-joins-e-normalizacao/3.1-funcoes-mais-usadas-no-sql/paraFixar1.sql
@@ -0,0 +1,14 @@
+-- tarefa 1
+SELECT UCASE('trybe');
+
+-- tarefa 2
+SELECT REPLACE('Você já ouviu falar do DuckDuckGo?', 'DuckDuckGo', 'Google');
+
+-- tarefa 3
+SELECT CHAR_LENGTH('Uma frase qualquer');
+
+-- tarefa 4
+SELECT SUBSTRING('A linguagem JavaScript está entre as mais usadas', 13, 10);
+
+-- tarefa 5
+SELECT LCASE('RUA NORTE 1500, SÃO PAULO, BRASIL');
