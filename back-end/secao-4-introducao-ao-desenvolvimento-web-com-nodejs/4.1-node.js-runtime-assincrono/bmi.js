98d57ce7bab6b4a0bc7893e3fd38f99145bc0b9f
Natalia Augusto <nat_augusto@hotmail.com>
2022-12-25 16:43:53 -0300

    Exercicio 5

--- back-end/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/4.1-node.js-runtime-assincrono/bmi.js
+++ back-end/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/4.1-node.js-runtime-assincrono/bmi.js
@@ -6,7 +6,25 @@ const height = readline.questionFloat('What\'s your height in m?');
 const calculateBmi = () => {
   const bmi = weight / (height * height);
 
-  return console.log(`BMI: ${bmi.toFixed(2)}`);
+  return bmi.toFixed(2);
 };
 
-calculateBmi();
+const condition = () => {
+  const bmi = calculateBmi();
+
+  if (bmi < 18.5) return 'Underweight (thin)';
+  if (bmi >= 18.5 && bmi < 25) return 'Normal weight';
+  if (bmi >= 25 && bmi < 30) return 'Overweight';
+  if (bmi >= 30 && bmi < 35) return 'Grade I obesity';
+  if (bmi >= 35 && bmi < 40) return 'Grade II obesity';
+  if (bmi >= 40) return 'Obesity grades III and IV';
+};
+
+const main = () => {
+  const bmi = calculateBmi();
+  const message = condition();
+
+  return console.log(`BMI: ${bmi}, ${message}`);
+};
+
+main();
