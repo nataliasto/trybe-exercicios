76810fb6ab0431529d9b59b79d79d3f996de3934
Natalia Augusto <nat_augusto@hotmail.com>
2023-01-04 00:45:43 -0300

    Exercício 4 - Criando função de leitura do arquivo json com fs

--- back-end/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/4.2-node.js-api-rest-com-express/movie-rental/src/app.js
+++ back-end/secao-4-introducao-ao-desenvolvimento-web-com-nodejs/4.2-node.js-api-rest-com-express/movie-rental/src/app.js
@@ -1,5 +1,19 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 
 const app = express();
+
+const moviesPath = path.resolve(__dirname, './movies.json');
+
+const readMovies = async () => {
+  try {
+  const data = await fs.readFile(moviesPath, 'utf-8');
+  const movies = JSON.parse(data);
+  return movies;
+  } catch (err) {
+    console.error(`Erro ao ler o arquivo: ${err.message}`);
+  }
+};
 
 module.exports = app;
